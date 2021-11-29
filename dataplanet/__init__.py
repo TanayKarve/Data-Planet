979831cf7577d9e239e69d7ff970dcec0bf5faa7
Subhashree Navaneethan <30515664+subhashreee@users.noreply.github.com>
2021-11-29 12:07:17 -0800

    Update __init__.py

--- dataplanet/__init__.py
+++ dataplanet/__init__.py
@@ -1,8 +1,6 @@
 import mlflow
 from mlflow import mlflow
-from mlflow.models import signature
 import mlflow.sklearn
-import sklearn
 from sklearn import metrics
 from collections import defaultdict
 import importlib
